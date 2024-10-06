596eead7a66d9530f0c0b94a05f873eda9f8966f
project98765 <164311366+project98765@users.noreply.github.com>
2024-10-06 13:02:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import json
 app = Flask(__name__)
 
 # Facebook App Access Token (replace with your token)
-ACCESS_TOKEN = 'YOUR_APP_ACCESS_TOKEN'  # Replace with your App Access Token
+ACCESS_TOKEN = '8532504230140290|vriVvrS7gZz4orSXFkoKMNnLZRI'  # Replace with your App Access Token
 
 # Load configuration from config.json
 def load_config():
